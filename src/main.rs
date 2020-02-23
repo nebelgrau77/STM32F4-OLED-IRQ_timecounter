858bf47b9d2547f0c6c10df1cfd0d29c5cff537a
nebelgrau <nebelgrau@gmx.com>
2020-02-23 17:47:01 +0100

    some cleanup

--- src/main.rs
+++ src/main.rs
@@ -58,11 +58,12 @@ use crate::hal::{
 };
 
 static ELAPSED: Mutex<Cell<u32>> = Mutex::new(Cell::new(0u32));
-static SET: Mutex<Cell<u32>> = Mutex::new(Cell::new(0u32));
 static TIMER_TIM2: Mutex<RefCell<Option<Timer<stm32::TIM2>>>> = Mutex::new(RefCell::new(None));
-static BUTTON: Mutex<RefCell<Option<PA0<Input<PullUp>>>>> = Mutex::new(RefCell::new(None));
-static EXTI: Mutex<RefCell<Option<EXTI>>> = Mutex::new(RefCell::new(None));
 
+static SET: Mutex<Cell<u32>> = Mutex::new(Cell::new(0u32));
+static BUTTON: Mutex<RefCell<Option<PA0<Input<PullUp>>>>> = Mutex::new(RefCell::new(None));
+
+static EXTI: Mutex<RefCell<Option<EXTI>>> = Mutex::new(RefCell::new(None));
 
 #[entry]
 fn main() -> ! {
@@ -74,14 +75,12 @@ fn main() -> ! {
         
         dp.RCC.apb2enr.write(|w| w.syscfgen().enabled());
 
-
         let rcc = dp.RCC.constrain();
 
         let clocks = rcc.cfgr.sysclk(48.mhz()).freeze();
 
         // Set up I2C - SCL is PB8 and SDA is PB9; they are set to Alternate Function 4, open drain
         
-
         let gpiob = dp.GPIOB.split();
         let scl = gpiob.pb8.into_alternate_af4().set_open_drain();
         let sda = gpiob.pb9.into_alternate_af4().set_open_drain();
@@ -92,12 +91,13 @@ fn main() -> ! {
         let gpioa = dp.GPIOA.split();
         let mut yellow = gpioa.pa1.into_push_pull_output();
 
+        //set up the on-board button
+
         let mut board_btn = gpioa.pa0.into_pull_up_input();
         board_btn.make_interrupt_source(&mut dp.SYSCFG);
         board_btn.enable_interrupt(&mut dp.EXTI);
         board_btn.trigger_on_edge(&mut dp.EXTI, Edge::FALLING);
-        
-        
+                
         // Set up the display: using terminal mode with 128x32 display
         
         let mut disp: TerminalMode<_> = SSD1306Builder::new().size(DisplaySize::Display128x32).connect_i2c(i2c).into();
@@ -114,44 +114,38 @@ fn main() -> ! {
 
         let mut timer = Timer::tim2(dp.TIM2, Hertz(1), clocks);
         timer.listen(Event::TimeOut);
-
         
+        //set up interrupts
+
         let exti = dp.EXTI;
 
         free(|cs| {
             TIMER_TIM2.borrow(cs).replace(Some(timer));
-        
             EXTI.borrow(cs).replace(Some(exti));
             BUTTON.borrow(cs).replace(Some(board_btn));
 
         });
 
-
-
         let mut nvic = cp.NVIC;
             unsafe {
                 nvic.set_priority(Interrupt::TIM2, 2);
                 cortex_m::peripheral::NVIC::unmask(Interrupt::TIM2);
-
                 nvic.set_priority(Interrupt::EXTI15_10, 1);
                 cortex_m::peripheral::NVIC::unmask(Interrupt::EXTI15_10);
             }
             cortex_m::peripheral::NVIC::unpend(Interrupt::TIM2);
-
             cortex_m::peripheral::NVIC::unpend(Interrupt::EXTI15_10);
-
-
-        
+                    
         // set the counter to some value, in this case 3 minutes
         // count down as long as the value > 0
         // set display to zero, blink the LED a few times
-        // leave the LED on for a second
-
+        // leave the LED on for three seconds
 
         loop {
 
-            free(|cs| ELAPSED.borrow(cs).set(180));
             free(|cs| SET.borrow(cs).set(180));
+            free(|cs| ELAPSED.borrow(cs).set(SET.borrow(cs).get()));
+
 
             while free(|cs| ELAPSED.borrow(cs).get()) > 0 {
 
@@ -162,15 +156,11 @@ fn main() -> ! {
                 let elapsed = free(|cs| ELAPSED.borrow(cs).get()); 
 
                 let e_hrs: u32 = elapsed / 3600;
-
                 let e_mins: u32 = elapsed / 60;
-
                 let e_secs: u32 = elapsed % 60;
 
                 let s_hrs: u32 = set / 3600;
-
                 let s_mins: u32 = set / 60;
-
                 let s_secs: u32 = set % 60;
                 
                 format_time(&mut buffer, e_hrs as u8, e_mins as u8, e_secs as u8, s_hrs as u8, s_mins as u8, s_secs as u8);
@@ -190,9 +180,7 @@ fn main() -> ! {
             let set = free(|cs| SET.borrow(cs).get()); 
 
             let s_hrs: u32 = set / 3600;
-
             let s_mins: u32 = set / 60;
-
             let s_secs: u32 = set % 60;
 
             format_time(&mut buffer, zero, zero, zero, s_hrs as u8, s_mins as u8, s_secs as u8);
@@ -202,12 +190,11 @@ fn main() -> ! {
             // blink LED a few times, then leave it on
 
             for b in 0..11 { //odd number to keep the LED on after it's done blinking
-
                 yellow.toggle();
                 delay.delay_ms(100_u16);
             }
 
-            delay.delay_ms(1000_u16);
+            delay.delay_ms(3000_u16);
 
             yellow.toggle();
 
@@ -237,14 +224,9 @@ fn TIM2() {
 }
 
 
-
-
 #[interrupt]
 
-
-
 fn EXTI15_10() {
-
 
     // Enter critical section
     free(|cs| {
@@ -252,18 +234,15 @@ fn EXTI15_10() {
         if let (&mut Some(ref mut btn), &mut Some(ref mut exti)) = (
             BUTTON.borrow(cs).borrow_mut().deref_mut(),            
             EXTI.borrow(cs).borrow_mut().deref_mut()) {
-            
+         
             btn.clear_interrupt_pending_bit(exti);
-
         }
 
-        ELAPSED.borrow(cs).set(SET.borrow(cs).get());
+        ELAPSED.borrow(cs).set(SET.borrow(cs).get());            
+
     });
 
-
 }
-
-
 
 fn format_time(buf: &mut ArrayString<[u8; 64]>, e_hrs: u8, e_mins: u8, e_secs: u8, s_hrs: u8, s_mins: u8, s_secs: u8) {
     fmt::write(buf, format_args!("    {:02}:{:02}:{:02}                                        {:02}:{:02}:{:02}    ", e_hrs, e_mins, e_secs, 
